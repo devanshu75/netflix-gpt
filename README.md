41d645abf0be4208f13495f60b93f72244dc8cbc
devanshu75 <devanshurana75@gmail.com>
2025-11-01 12:12:43 +0530

    login form added

--- README.md
+++ README.md
@@ -2,19 +2,22 @@
 
 - Create React App
 - Configured TailwindCSS
-
+- Header
+- Login Form
+- Sign Up Form
 
 # Features
+
 - Login/Sign Up
-     - Sing In / Sing Up Form
-     - redirect to Browse Page
+  - Sing In / Sing Up Form
+  - redirect to Browse Page
 - Browse (after authentication)
-   - Header
-   - Main Movie
-     - Trailer in Background
-     - title and Description
-     - Movie Suggestions
-        - MovieLists
+  - Header
+  - Main Movie
+    - Trailer in Background
+    - title and Description
+    - Movie Suggestions
+      - MovieLists
 - NetflixGPT
-   - Search Bar
-   - Movie Suggestions
+  - Search Bar
+  - Movie Suggestions
---
+++ src/components/Login.jsx
@@ -0,0 +1,31 @@
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/9c363af5-4750-4f14-87d1-8125f5276db0/web/IN-en-20251027-TRIFECTA-perspective_b68b1528-3a10-4997-9f99-48ccbdb86626_large.jpg"
+          alt="logo"
+        />
+      </div>
+      <form className="w-3/12 absolute my-36 mx-auto right-0 left-0 p-12 bg-black text-white rounded-lg opacity-85">
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-600"
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          className="p-4  my-4 w-full bg-gray-600"
+        />
+        <button className="py-4 my-4 bg-red-700 w-full rounded-lg">Sign In</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
